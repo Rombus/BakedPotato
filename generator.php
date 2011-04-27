e5d29a16f7a8b734bbb92a8d60174db737c15b02
Rick Kosbab <rick@romb.us>
2011-04-27 15:59:26 -0400

    POC 2, Now with proper HTML!

--- generator.php
+++ generator.php
@@ -11,24 +11,39 @@
  // setting up directory to search for new posts
  $input_dir = "./Input/";
  $output_dir = "./Output/";
+ $template = "./Template/XHTML1_STRICT.html";
  $files1 = scandir($input_dir);
  
  // scanning though the array given to find just .txt files and run them though markdown
  foreach($files1 as $key => $value){
 	if(preg_match('/.*.txt/',$value)) {
+		
+		// Opens the .txt file and copys the contents
 		$input_filename = $input_dir . $value;
 		$input_handle = fopen($input_filename, "r");
 		$input_content = fread($input_handle, filesize($input_filename));
 		fclose($input_handle);
+		
+		// runs the markdown script
 		$parsed_html = Markdown($input_content);
-		$output_filename = $output_dir . $value;
+		
+		// Opening the Template file
+		$template_handle = fopen($template, "r");
+		$template_content = fread($template_handle, filesize($template));
+		fclose($template_handle);
+		
+		// Put the parsed_html in between the <body> tags
+		$finished_html = str_replace("<body>" , "<body>\n" . $parsed_html , $template_content );
+		
+		// For testing, will be labled something specific, just trying html rename
+		$output_filename = $output_dir . basename($input_filename, ".txt") . '.html';
 		$output_handle = fopen($output_filename, "c");
-		if( fwrite($output_handle, $parsed_html) === FALSE) { 
-			echo "Cannot Write to File($output_filename)";
+		if( fwrite($output_handle, $finished_html) === FALSE) { 
+			echo "Cannot Write to File( $output_filename )";
 			exit;
 		}
 		fclose($output_handle);
-		echo "Sucess, wrote to file ($output_filename)";
+		echo "Sucess, wrote to file ( $output_filename )";
 		}
 }
  ?>
